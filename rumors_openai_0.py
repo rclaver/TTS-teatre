a381f33b1b084bcd7166958370e14c11a3431917
rclaver <rclaver@xtec.cat>
2024-09-16 11:19:30 +0200

    merge

--- rumors_openai_0.py
+++ rumors_openai_0.py
@@ -6,7 +6,6 @@
 '''
 from openai import OpenAI
 
-PROJECT_ID=""
 f = open("API_Key_ElevenLabs", "r")
 eAK = f.read()
 f.close()
@@ -16,16 +15,8 @@ f.close()
 f = open("org_OpenAI", "r")
 org = f.read()
 f.close()
-<<<<<<< HEAD
 
 client = OpenAI(
    organization=org,
    project='$PROJECT_ID'
-=======
-
-
-client = OpenAI(
-   organization=org,
-   #project='$PROJECT_ID'
->>>>>>> 0cd8cc7fa6f588eef23da911c1c1e61f2f542b68
 )
